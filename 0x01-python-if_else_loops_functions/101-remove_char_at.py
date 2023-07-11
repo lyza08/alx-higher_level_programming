78fe1accf17cb6af0479ac937482df63d77d80d9
lyza08 <oppongliza18@gmail.com>
2023-07-11 15:22:54 -0700

    debugg

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,10 +1,6 @@
 #!/usr/bin/python3
-# 101-remove_char_at.py
-
-
-def remove_char_at(str, n):
-    """Create a copy of the string without the character at position n."""
-    if n < 0:
-        return (str)
-    return (str[:n] + str[n+1:])
-
+for i in range(26):
+    if i % 2 == 0:
+        print('{:c}'.format(122 - i), end='')
+    else:
+        print('{:c}'.format(90 - i), end='')
--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -1,40 +1,34 @@
 #include "lists.h"
 
+/**
+ * insert_node - Inserts a number into a sorted singly-linked list.
+ * @head: A pointer the head of the linked list.
+ * @number: The number to insert.
+ *
+ * Return: If the function fails - NULL.
+ * Otherwise - a pointer to the new node.
+ */
 listint_t *insert_node(listint_t **head, int number)
 {
-	listint_t *new_node, *current_node;
-	if (head == NULL)
-		return NULL;
+	listint_t *node = *head, *new;
 
-	new_node = malloc(sizeof(listint_t));
-
-	if (new_node == NULL)
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
 		return (NULL);
-	current_node = *head;
-	new_node->n = number;
-	new_node->next = NULL;
-	if (*head == NULL)
+	new->n = number;
+
+	if (node == NULL || node->n >= number)
 	{
-		*head = new_node;
-		return (new_node);
-	}
-	if (current_node->n > number)
-	{
-		new_node->next = current_node;
-		*head = new_node;
-		return (new_node);
+		new->next = node;
+		*head = new;
+		return (new);
 	}
 
-	while (current_node->next != NULL)
-	{
-		if(current_node->next->n > number)
-		{
-			new_node->next = current_node->next;
-			current_node->next = new_node;
-			return new_node;
-		}
-		current_node = current_node->next;
-	}
-	current_node->next = new_node;
-	return (new_node);
+	while (node && node->next && node->next->n < number)
+		node = node->next;
+
+	new->next = node->next;
+	node->next = new;
+
+	return (new);
 }
